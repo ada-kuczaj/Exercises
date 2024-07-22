2aa1eb50011239e758164109db9e55ba4801983e
Adriana Kuczaj <ada.kuczaj@gmail.com>
2024-07-22 23:27:13 +0200

    stack and mapping

--- exercism-exercises/29-matching-brackets/matchingBrackets.js
+++ exercism-exercises/29-matching-brackets/matchingBrackets.js
@@ -1,34 +1,28 @@
 function isPaired(text) {
-    for (let i = 0; i < text.length; i++) {
 
-        //checking the number of pairs of brackets out of correct order
-        bracketsNumber = 0; // []
-        bracesNumber = 0; // {}
-        parenthesesNumber = 0; // ()
+    const stack = [];
+    // the order of the brackets is important
+    // because later we check whether the closing bracket has a corresponding opening bracket on the stack
+    const pairs = {
+        ']': '[',
+        '}': '{',
+        ')': '('
+    };
 
-        if (text[i] === '[') {
-            bracketsNumber += 1;
+    for (let char of text) {
+        if (['[', '{', '('].includes(char)) {
+            stack.push(char);
         }
-        if (text[i] === '{') {
-            bracesNumber += 1;
+        else if ([']', '}', ')'].includes(char)) {
+            //checks whether the last opening brace on the stack matches the current closing brace
+            if (stack.pop() !== pairs[char]) {
+                return false;
+            }
         }
-        if (text[i] === '(') {
-            parenthesesNumber += 1;
-        }
-
-
-        if (text[i] === ']') {
-            bracketsNumber -= 1;
-        }
-        if (text[i] === '}') {
-            bracesNumber -= 1;
-        }
-        if (text[i] === ')') {
-            parenthesesNumber -= 1;
-        }
-
     }
-    return (bracketsNumber === 0 && bracesNumber === 0 && parenthesesNumber === 0) ? true : false;
+
+    // if the stack is empty, it means all the brackets have been paired correctly
+    return stack.length === 0;
 }
 
 module.exports = {
